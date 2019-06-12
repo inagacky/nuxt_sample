ae51a2ab8529489d79b8e34fa219fcd1b9962615
daisuke-inagaki <chanc.euse.0302@gmail.com>
2019-06-12 14:54:21 +0900

    first commit

---
+++ sample-project/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <myheader></myheader>
+    <p v-if="msg.length > 0">
+      {{msg}}
+    </p>
+    <p v-else>
+      no text
+    </p>
+    <input type="text" v-model="msg">
+    <button @click="clear()">clear</button>
+  </div>
+</template>
+
+<script>
+import myheader from './components/myheader.vue'
+
+export default {
+  components: {
+    myheader
+  },
+  data () {
+    return {
+      msg: 'Hello World!'
+    }
+  },
+  methods: {
+    clear () {
+      this.msg = ''
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
